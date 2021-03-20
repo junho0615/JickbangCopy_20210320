edfda822e5cdc870ad03894952085bb10be9c0a5
홍준호 <0615wnsgh@gmail.com>
2021-03-20 15:39:34 +0900

    가격도 만들어봄

--- app/src/main/java/com/neppplus/jickbangcopy_20210320/datas/Room.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210320/datas/Room.kt
@@ -1,10 +1,31 @@
 package com.neppplus.jickbangcopy_20210320.datas
 
+import java.text.NumberFormat
+import java.util.*
+
 class Room(
     val price : Int,
     val address : String,
     val floor : Int,
     val description : String) {
+
+    fun getFormattedPrice() : String {
+
+        if (this.price < 10000) {
+            val priceStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
+            return priceStr
+        }
+        else {
+            val uk = this.price / 10000
+
+            val rest = this.price % 10000
+
+            val priceStr = "${uk}억${NumberFormat.getNumberInstance(Locale.KOREA).format(rest)}"
+
+            return priceStr
+
+        }
+    }
 
     fun getFormattedFloor() : String {
 
@@ -18,4 +39,7 @@ class Room(
             return "지하${-this.floor}층"
         }
     }
+
+
+
 }
