2f4d6fc65f088255857281ae6818528cc007c56e
홍준호 <0615wnsgh@gmail.com>
2021-03-20 16:07:31 +0900

    넘겨준 방 데이터를 Room 형태로 변환해서 사용

--- app/src/main/java/com/neppplus/jickbangcopy_20210320/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210320/ViewRoomDetailActivity.kt
@@ -2,10 +2,20 @@ package com.neppplus.jickbangcopy_20210320
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20210320.datas.Room
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData = intent.getSerializableExtra("roomInfo") as Room
+
+        priceTxt.text = roomData.getFormattedPrice()
+        floorTxt.text = roomData.getFormattedPrice()
+        addressTxt.text = roomData.address
+        descTxt.text = roomData.description
+
     }
 }
